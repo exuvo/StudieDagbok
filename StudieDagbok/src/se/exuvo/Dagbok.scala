18bdebdd03048256e15f920200c9c8ea6caa54ef
exuvo <exuvo@exuvo.se>
2012-09-14 00:49:02 +0200

    Program works now

--- StudieDagbok/src/se/exuvo/Dagbok.scala
+++ StudieDagbok/src/se/exuvo/Dagbok.scala
@@ -1,12 +1,46 @@
 package se.exuvo
+import java.text.DateFormat
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
 
-class Entry {
-  val date
-  val text: String
+class Entry(val date: Date, val text: String) {
 }
 
-class Dagbok(fil) {
-	val file
-	val entries: Array[Entry]
-	
+class Dagbok(val name: String, val entries: Array[Entry]) {
+  
+  def getByDate(date: Date) : Option[Entry] = {
+    val en = entries.filter(e => e.date.getYear() == date.getYear() && e.date.getMonth() == date.getMonth() && e.date.getDate() == date.getDate())
+    if(en.length > 0) Option(en(0))
+    else Option(null)
+  }
+}
+
+object Dagbok{
+  def apply(filename: String): Dagbok = {
+	  val source = scala.io.Source.fromFile(filename)
+	   
+	  val datePattern = "([0-9]{4}-[0-9]{1,2}-[0-9]{1,2})".r
+	  val dateFormat = new SimpleDateFormat("yyy-MM-dd")
+	  var date: Date = null
+	  var text: StringBuilder = new StringBuilder(512);
+	  var entries: Array[Entry] = Array[Entry]()
+		  
+	  val lines = source.getLines().foreach(l => l match{
+	    case datePattern(d) => {
+	      if(text.length > 0) {
+	        entries = entries :+ new Entry(date, text.toString())
+	        text.clear()
+	      }
+	    	date = dateFormat.parse(d)
+	    }
+	    case t => text.append(t + "\n")
+	  })
+	  if(text.length > 0) {
+        entries = entries :+ new Entry(date, text.toString())
+    }
+	  source.close()
+	  
+	  new Dagbok(filename, entries)
+	}
 }
--- StudieDagbok/src/se/exuvo/main.scala
+++ StudieDagbok/src/se/exuvo/main.scala
@@ -1,4 +1,7 @@
 package se.exuvo
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Calendar
 
 /**
  * Program för att läsa tidigare dagar och repetera dom
@@ -6,17 +9,52 @@ package se.exuvo
  * Användaren får skriva in ny dag med valfri text editor i botten av filen
  */
 object main {
+  val dateFormat = new SimpleDateFormat("yyy-MM-dd")
   
   def help(){
     println("noob")
+  }
+  
+  def p(e : Entry){
+    println(dateFormat.format(e.date) + "\n" + e.text)
+  }
+  
+  def p(bok: Dagbok, date: Date){
+    println(dateFormat.format(date))
+    bok.getByDate(date) match {
+      case Some(entry) => p(entry);
+      case None => println("No entry")
+    }
   }
 	
   def main(args: Array[String]) = {
     args.foreach(f => 
      f match {
       case "-h" | "--help" => help()
+      case _ =>
     })
     
+//    val dagbok = Dagbok(args(args.length -1))
+    val d = Dagbok("sdg.txt")
+//    println(d.name + " contains " + d.entries.length + " entries")
+//    d.entries.foreach(e => p(e))
     
+    val day = Calendar.getInstance()
+    day.setTime(new Date())
+    
+    println("Todays entry:")
+    p(d, day.getTime())
+    
+    println("Yesterdays entry:")
+    day.add(Calendar.DATE, -1)
+    p(d, day.getTime())
+    
+    println("Previous weeks entry:")
+    day.add(Calendar.DATE, -6)
+    p(d, day.getTime())
+    
+    println("Previus month entry:")
+    day.add(Calendar.DATE, -23)
+    p(d, day.getTime())
   }
 }
